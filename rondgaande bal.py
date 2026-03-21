c9ff99e723aae442f4fb620285a80f1787b925d8
seppe <vangompel.seppe@provilion.be>
2026-03-21 11:36:40 +0100

    Laat de cirkel botsen tegen vier kanten

--- rondgaande bal.py
+++ rondgaande bal.py
@@ -6,9 +6,12 @@ pygame.init()
 win = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-x = 50
+x = 300
 y = 200
-speed = 5
+radius = 50
+
+speed_x = 5
+speed_y = 4
 
 run = True
 while run:
@@ -18,17 +21,18 @@ while run:
         if e.type == QUIT or (e.type == KEYDOWN and e.key == K_BACKSPACE):
             run = False
 
+    x += speed_x
+    y += speed_y
 
-    x += speed
-    if x + 50 > 800:
-        speed = -speed
-    if x - 50 < 0:
-        speed = -speed
+    if x + radius > 800 or x - radius < 0:
+        speed_x *= -1
+
+    if y + radius > 600 or y - radius < 0:
+        speed_y *= -1
 
     win.fill((0, 0, 0))
 
-
-    pygame.draw.circle(win, (255, 0, 0), (x, y), 50)
+    pygame.draw.circle(win, (255, 0, 0), (x, y), radius)
 
     pygame.display.flip()
 
